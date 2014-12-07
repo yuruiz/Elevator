e20331482417ae4f4faba23a954d4efc003e7b1f
Zhou Yurui <accountregistor@gmail.com>
2014-12-06 23:52:52 -0500

    might worked

--- code/simulator/elevatorcontrol/DriveControl.java
+++ code/simulator/elevatorcontrol/DriveControl.java
@@ -245,7 +245,6 @@ public class DriveControl extends Controller {
 
 			if (allClosed) {
 				ClosedCount++;
-				// System.out.println("Closed Count " + ClosedCount);
 			} else {
 				ClosedCount = 0;
 			}
@@ -269,7 +268,7 @@ public class DriveControl extends Controller {
 			// #transition 'DC.T.5'
 			if (allClosed && mCarWeight.getValue() < Elevator.MaxCarCapacity
 					&& atFloor.getCurrentFloor() != mDesiredFloor.getFloor()
-					&& ClosedCount > 20 && driveSpeedPayload.speed() == 0) {
+					&& ClosedCount > 35 && driveSpeedPayload.speed() == 0) {
 				log("stop to slow");
 				ClosedCount = 0;
 				newState = State.SLOW;
